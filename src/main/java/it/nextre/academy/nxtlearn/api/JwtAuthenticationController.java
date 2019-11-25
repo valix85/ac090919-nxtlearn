bd1af024356c23edf5e2698d17b916aa6073bf6e
valix85@gmail.com <valix85@gmail.com>
2019-11-25 16:27:58 +0100

    add jwt authentication

--- src/main/java/it/nextre/academy/nxtlearn/api/JwtAuthenticationController.java
+++ src/main/java/it/nextre/academy/nxtlearn/api/JwtAuthenticationController.java
@@ -1,6 +1,7 @@
 package it.nextre.academy.nxtlearn.api;
 
 
+import it.nextre.academy.nxtlearn.config.CustomUserDetails;
 import it.nextre.academy.nxtlearn.dto.JwtRequestDto;
 import it.nextre.academy.nxtlearn.dto.JwtResponseDto;
 import it.nextre.academy.nxtlearn.myutils.JwtTokenUtil;
@@ -8,9 +9,14 @@ import it.nextre.academy.nxtlearn.service.impl.CustomUserDetailsServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.*;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -43,7 +49,23 @@ public class JwtAuthenticationController {
         final UserDetails userDetails = userDetailsService
                 .loadUserByUsername(authenticationRequest.getUtente());
         final String token = jwtTokenUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new JwtResponseDto(token));
+        // hook here to add custom properties
+        // todo spostare in un service, i controller devon essere leggeri
+        Map<String, Object> risp = new HashMap<>();
+        risp.put("token", token);
+        risp.put("username",userDetails.getUsername());
+        String role = "";
+        role = userDetails.getAuthorities().stream()
+                .map(el -> ((GrantedAuthority) el).getAuthority())
+                .collect(Collectors.joining( "," ));
+        role = role.replaceAll("ROLE_","");
+        risp.put("role",role);
+
+        risp.put("expireSession", jwtTokenUtil.getExpirationDateFromToken(token));
+        risp.put("nome", ((CustomUserDetails)userDetails).getPersona().getNome());
+        risp.put("cognome",((CustomUserDetails)userDetails).getPersona().getCognome());
+
+        return ResponseEntity.ok(risp);
     }
 
     private void authenticate(String username, String password) throws Exception {
--- src/main/java/it/nextre/academy/nxtlearn/myutils/JwtTokenUtil.java
+++ src/main/java/it/nextre/academy/nxtlearn/myutils/JwtTokenUtil.java
@@ -46,6 +46,7 @@ public class JwtTokenUtil implements Serializable {
     //generate token for user
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
+        claims.put("CustomProperty in JWT","myValue1");
         return doGenerateToken(claims, userDetails.getUsername());
     }
     //while creating the token -
